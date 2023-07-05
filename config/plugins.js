9f9286f589eaa72d215a8c956c5a8cec78c4e21f
Raza Mughal <razasabir23@outlook.com>
2023-07-05 13:56:43 +0500

    installed plugins

---
+++ config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
